ba9cfc42cb38fcf5380f959da0c784ea2123c4d8
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-08-13 22:18:26 -0300

    login e cadastro

---
+++ config.js
@@ -0,0 +1,12 @@
+
+
+
+  var config = {
+    apiKey: "AIzaSyC78bEhDFxI36OmTB-MTBTRBL7NyEJbTEs",
+    authDomain: "redemulher-409a6.firebaseapp.com",
+    databaseURL: "https://redemulher-409a6.firebaseio.com",
+    projectId: "redemulher-409a6",
+    storageBucket: "redemulher-409a6.appspot.com",
+    messagingSenderId: "605105618143"
+  };
+  firebase.initializeApp(config);
