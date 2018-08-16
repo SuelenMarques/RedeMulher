3c0c32de984696b8aff40168a43bddb588bea2a2
SuelenMarques <39528149+SuelenMarques@users.noreply.github.com>
2018-08-16 17:59:39 -0300

    Fechou

--- public/loginscreen.js
+++ public/loginscreen.js
@@ -7,43 +7,40 @@ $(document).ready(function() {
   $(".sign-in-button").click(signInClick);
 });
 
-function signUpClick(event) {
-  event.preventDefault();
-  var name = $(".sign-name").val();
-  var email = $(".sign-email").val();
-  var password = $(".sign-password").val();
+  function signUpClick(event) {
+    event.preventDefault();
+    var name = $(".sign-name").val();
+    var email = $(".sign-email").val();
+    var password = $(".sign-password").val();
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(function(response) {
       alert("Cadastro concluido com sucesso !!! Faça seu login\n " + name);
-
-
-    })
-    .catch(function(error) {
-      checkError(error);
-    });
-
-}
-
-function signInClick(event) {
-  event.preventDefault();
-  var name = $(".sign-name").val();
-  var email = $(".sign-email").val();
-  var password = $(".sign-password").val();
-
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function(response) {
-      var userId = response.user.uid;
-      database.ref("users/" + userId).set({
-       name: name,
-       email: email
-     });
-      redirectToPage(userId);
     })
     .catch(function(error) {
       checkError(error);
     });
 }
+
+  function signInClick(event) {
+    event.preventDefault();
+    var name = $(".sign-name").val();
+    var email = $(".sign-email").val();
+    var password = $(".sign-password").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(function(response) {
+        var userId = response.user.uid;
+        database.ref("users/" + userId).set({
+         name: name,
+         email: email
+       });
+        redirectToPage(userId);
+      })
+      .catch(function(error) {
+        checkError(error);
+      });
+  }
 
 function checkError(error) {
   var errorMessage = error.message;
--- public/network.js
+++ public/network.js
@@ -1,7 +1,5 @@
 var database = firebase.database();
 var USER_ID = window.location.search.match(/\?userId=(.*)/)[1];
-console.log(USER_ID);
-
 $(document).ready(function() {
 createUsersDB();
   getMsgDB();
@@ -20,29 +18,39 @@ createUsersDB();
 
 function createListItem(text, key) {
   $(".news-list").append(`
-    <li class="users-list-li">
-    <span data-text-id="${key}">${text}</span><br>
-     
-        <button class="editar" data-edit-id="${key}">Editar </button>
-         <button class="deletar data-delete-id="${key}">Excluir </button>
-      
+    <li>
+      <span data-text-id="${key}">${text}</span>
+      <button class="deletar" data-delete-id="${key}">Excluir </button>
+      <button class="editar" data-edit-id="${key}">Editar </button>
+      <button class="gostar" data-like-id="${key}">Gostei </button>
     </li>`
     );
 
-  $(`button[data-delete-id=${key}]`).click(function() {
-    $(this).parent().remove();
-    database.ref('posts/'+ USER_ID + "/" + key).remove();
-  });
-
-  $(`button[data-edit-id=${key}]`).click(function() {
+    $(`button[data-delete-id=${key}]`).click(function() {
+        $(this).parent().remove();
+        database.ref('posts/'+ USER_ID + "/" + key).remove();
+      });
+    $(`button[data-delete-id=${key}]`).click(function() {
+        $(this).parent().remove();
+        database.ref('posts/'+ USER_ID + "/" + key).remove();
+      });
+    $(`button[data-edit-id=${key}]`).click(function() {
     var newText = prompt(`Altere o seu texto: ${text}`);
-    $(`span[data-text-id=${key}]`).text(newText);
+      $(`span[data-text-id=${key}]`).text(newText);
     database.ref(`posts/${USER_ID}/${key}`).update({
       text: newText
+      })
+    });
+    var likes = 1;
+    $(`button[data-like-id=${key}]`).click(function() {
+      function like(){
+        return likes ++;
+      }
+      database.ref('posts/'+ USER_ID + "/" + key).set({
+        likes: like()
+      })
     })
-
-  });
-}
+  }
 
 function getMsgDB() {
   database.ref("posts/" + USER_ID).once('value')
@@ -50,7 +58,8 @@ function getMsgDB() {
       snapshot.forEach(function(childSnapshot) {
         var childKey = childSnapshot.key;
         var childData = childSnapshot.val();
-        createListItem(childData.text, childKey)
+        createListItem(childData.text, childKey);
+
       });
     });
 }
@@ -68,7 +77,15 @@ function getUsersDB(){
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
-      createUsers(childData.name, childKey);
+      createUsers(childData.name, childKey)
+    });
+  });
+  database.ref("postsFriend").once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
+      createPostsFriend(childData.text, childKey)
     });
   });
 }
@@ -89,35 +106,36 @@ function createUsers(name, key) {
     });
   })
 }
-// });
-//   $(`button[data-user-id=${key}]`).click(function () {
-//     database.ref('friendship/' + USER_ID).push({
-//       friendId: key
-//     });
-//   })
 
-// }
+function createPostsFriend(text, key) {
+  if (key !== USER_ID) {
+    $(".news-list").append(`
+      <li class="users-list-li-u">
+        <span>${text}</span>
+      </li>
+    `);
+  }
 
-//     var uploader = $("#uploader").val();
-//     var uploadButton = $("#upload-button");
-//
-// $("#upload-button").change(files);
-// function files(e){
-//   var file = e.target.files[0];
-//   var storageRef = firebase.storage().ref("gifts/" + file.name).
-//   var bar = storage.ref.put(file),
-//   bar.on("state_changed",
-//   function progress(snapshot) {
-//     var percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//     uploader.value = percent;
-//
-//   }
-//   function error(err){
-//
-//   }
-//   function complete(){
-//
-//   }
-//
-// )
-// }
+  $(`button[data-user-id=${key}]`).click(function () {
+    database.ref('postsFriend/' + USER_ID).push({
+    postsFriend: text
+    });
+
+})
+}
+
+var uploader = $("#uploader").val();
+var uploadButton = $("#upload-button");
+
+$("#upload-button").change(files);
+function files(e){
+  var file = e.target.files[0];
+  var storageRef = firebase.storage().ref("gifts/" + file.name);
+  var task = storageRef.put(file);
+  task.on('state_changed',
+    function progress(snapshot) {
+      var percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploader.value = percent;
+
+    }
+  )}
